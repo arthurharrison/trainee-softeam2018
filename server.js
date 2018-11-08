553c33ad12cbd94d2da874c30b253342537be020
Danielle Farias <danielle8farias@gmail.com>
2018-11-07 22:00:34 -0300

    desenvolvimento de rotas

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * Configuração do Setup
  * Arquivo: server.js
  * Author: Danielle Farias
+ * 
  */
 
  //chamadas dos pacotes:
@@ -17,16 +18,56 @@ mongoose.connect('mongodb://localhost/node-crud-api');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//==============================================================
+
 //porta onde será executada a API:
 var port = process.env.port || 8000;
 
 //criando instância das rotas via Express:
 var router = express.Router();
 
+router.use(function(req, res, next){
+    console.log('Algo está acontecendo aqui');
+    next();
+});
+
 //rota de exemplo:
 router.get('/', function(req, res){
     res.json({ message: 'Seja bem-vindo!'})
 });
+
+router.route('/gerente')
+
+//POST
+    .post(function(req, res) {
+        var gerente = new gerente();
+        
+        gerente.nome = req.body.nome;
+        gerente.cpf = req.body.cpf;
+        gerente.email = req.body.email;
+        gerente.permissao = req.body.permissao;
+
+        gerente.save(function(error) {
+            if(error)
+                res.send('Erro ao tentar salvar gerente... '+ error);
+
+            res.json({ message: 'Gerente cadastrado com sucesso!' });
+        });
+    })
+
+//GET ALL
+    .get(function(req, res) {
+        gerente.find(function(error, gerente) {
+            if(error)
+                res.send('Erro ao tentar selecionar os gerentes... '+ error);
+            
+            res.json(gerente);
+        });
+
+    });
+
+//GET
+
 
 //definindo padrão das rotas prefixadas:
 app.use('/api', router);
